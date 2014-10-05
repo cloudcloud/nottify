448ccb2ef0e0f07aa3464bdb34cf762b4aff6c0b
CerealBoy <allan.shone@yahoo.com>
2014-10-05 17:14:10 +1100

    Major clean-up and re-factor usage

--- src/app/controllers/nottify.go
+++ src/app/controllers/nottify.go
@@ -1,12 +1,9 @@
 package controllers
 
 import (
-	"go/build"
-	"path"
 	"strconv"
 
 	"code.google.com/p/go-sqlite/go1/sqlite3"
-	"github.com/cloudcloud/nottify/src/nottify"
 	"github.com/revel/revel"
 )
 
@@ -26,32 +23,7 @@ func (c Nottify) Home() revel.Result {
 		return c.Redirect(App.Index)
 	}
 
-	conf, db := c.loadConnection()
-	nott := nottify.Build(conf, db)
+	//nott := nottify.LoadConnection()
 
-	row := nott.LoadRandom(10)
-	return c.Render(row)
-}
-
-func (c Nottify) loadConnection() (*revel.MergedConfig, *sqlite3.Conn) {
-	gopath := build.Default.GOPATH
-	revel.ConfPaths = []string{path.Join(gopath, "src/github.com/cloudcloud/nottify/src/conf")}
-	config, err := revel.LoadConfig("app.conf")
-
-	if err != nil || config == nil {
-		panic("Failed to Config")
-	}
-
-	dsn, confErr := config.String("nottify.sqlite_path")
-	if confErr == false || dsn == "" {
-		panic("No database details have been defined")
-	}
-
-	db, err := sqlite3.Open(dsn)
-	if err != nil {
-		panic(err.Error())
-	}
-	//defer db.Close()
-
-	return config, db
+	return c.Render()
 }
